f1a366703519c360595ece95166fc88526bac5e8
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-11 23:24:36 +0530

    reading values from form created model for user

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-signup',
@@ -10,6 +11,28 @@ export class SignupComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signup(event : Event){
+    event.preventDefault();
+    console.log(event.target);
+    let form = <HTMLFormElement>event.target;
+    let name = (<HTMLInputElement>form.elements.namedItem("name")).value;
+    let email = (<HTMLInputElement>form.elements.namedItem("email")).value;
+    let password = (<HTMLInputElement>form.elements.namedItem("password")).value;
+    let phoneStr = (<HTMLInputElement>form.elements.namedItem("phone")).value;
+
+    let user : User = {
+      name, 
+      email,
+      password,
+      phone : parseInt(phoneStr)
+    }
+
+    console.log({
+      user
+    })
+   
   }
 
 }
